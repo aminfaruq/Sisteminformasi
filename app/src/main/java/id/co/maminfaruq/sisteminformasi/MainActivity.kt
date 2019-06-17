59a67f891e8a98353fbccb9e12c11c5367cb6152
amin faruq <aminfaruk.fa@gmail.com>
2019-06-17 14:45:14 +0700

    initial commit

--- app/src/main/java/id/co/maminfaruq/sisteminformasi/MainActivity.kt
+++ app/src/main/java/id/co/maminfaruq/sisteminformasi/MainActivity.kt
@@ -7,28 +7,51 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    var mHome = MenuFragment()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.frame_container, mHome)
+                .commit()
+
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+
+    }
+
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.navigation_home -> {
-                message.setText(R.string.title_home)
+
+                val fragment = MenuFragment()
+                supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.frame_container, fragment, fragment.javaClass.simpleName)
+                        .commit()
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_dashboard -> {
-                message.setText(R.string.title_dashboard)
+
+                val maps = MapsFragment()
+                supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.frame_container, maps, maps.javaClass.simpleName)
+                        .commit()
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_notifications -> {
-                message.setText(R.string.title_notifications)
+                val telp = TelpFragment()
+                supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.frame_container, telp, telp.javaClass.simpleName)
+                        .commit()
                 return@OnNavigationItemSelectedListener true
             }
         }
         false
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
-    }
 }
